196344d36554caa8243923a3c706037beea1d0a7
c22654545-afk <c22654545@gmail.com>
2025-10-03 12:18:23 +0200

    Update and rename script.js to config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// Config file
+const CONFIG = {
+  WEBHOOK_URL: "https://discord.com/api/webhooks/1421210194351886467/L0QB736hgCP1dElZpPwhcbgYkQP746rAYBaAUqWNEPhVJTFU8wvncnQrboQVzcQChBdL" // <- yaha apna Discord webhook URL dalna
+};
